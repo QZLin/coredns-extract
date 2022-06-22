e2002bdee63feea6358c080c2e73855731391327
QZLin <qzlin01@163.com>
2022-06-22 14:11:58 +0800

    u

--- actions.ps1
+++ actions.ps1
@@ -7,7 +7,7 @@ git clone https://gitclone.com/github.com/qzlin/forward.git
 # git clone local_cache/coredns
 # git clone local_cache/forward
 
-# wget https://raw.githubusercontents.com/newren/git-filter-repo/main/git-filter-repo -O git-filter-repo.py
+Invoke-WebRequest https://raw.githubusercontents.com/newren/git-filter-repo/main/git-filter-repo -OutFile git-filter-repo.py
 
 Push-Location coredns
 python ../git-filter-repo.py --path plugin/forward --subdirectory-filter plugin/forward  --force
