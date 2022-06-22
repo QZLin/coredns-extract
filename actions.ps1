02f7bc2d5092274aeb5c81b1a4f61241bcb57203
QZLin <qzlin01@163.com>
2022-06-22 20:20:59 +0800

    update

--- actions.ps1
+++ actions.ps1
@@ -1,12 +1,14 @@
 Push-Location $PSScriptRoot
+. ./clean.ps1
 
 # git clone https://github.com/coredns/coredns.git
 # git clone https://github.com/qzlin/forward.git
 
 # local test repo cache
-# git clone local_cache/coredns
-# git clone local_cache/forward
-
+if (Test-Path local_cache) {
+    git clone local_cache/coredns
+    git clone local_cache/forward
+}
 # Invoke-WebRequest https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo -OutFile git-filter-repo.py
 
 Push-Location coredns
