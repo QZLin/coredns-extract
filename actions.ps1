df2eacca1bc23936e2962d980dc3716715ac4a47
QZLin <qzlin01@163.com>
2022-06-22 15:23:29 +0800

    u

--- actions.ps1
+++ actions.ps1
@@ -1,13 +1,13 @@
 Push-Location $PSScriptRoot
 
-git clone https://gitclone.com/github.com/coredns/coredns.git
-git clone https://gitclone.com/github.com/qzlin/forward.git
+git clone https://github.com/coredns/coredns.git
+git clone https://github.com/qzlin/forward.git
 
 # local test repo cache
 # git clone local_cache/coredns
 # git clone local_cache/forward
 
-Invoke-WebRequest https://raw.githubusercontents.com/newren/git-filter-repo/main/git-filter-repo -OutFile git-filter-repo.py
+Invoke-WebRequest https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo -OutFile git-filter-repo.py
 
 Push-Location coredns
 python ../git-filter-repo.py --path plugin/forward --subdirectory-filter plugin/forward  --force
