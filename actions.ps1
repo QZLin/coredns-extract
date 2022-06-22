2e13b0ca337362ea5a68e5b7bf5b4a0851947728
QZLin <qzlin01@163.com>
2022-06-22 20:23:23 +0800

    update

--- actions.ps1
+++ actions.ps1
@@ -1,11 +1,11 @@
 Push-Location $PSScriptRoot
-. ./clean.ps1
 
 # git clone https://github.com/coredns/coredns.git
 # git clone https://github.com/qzlin/forward.git
 
 # local test repo cache
 if (Test-Path local_cache) {
+    . ./clean.ps1
     git clone local_cache/coredns
     git clone local_cache/forward
 }
