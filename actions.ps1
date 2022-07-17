62b539e4a7a78240361dae2f4d07de045ff8bff5
QZLin <qzlin01@163.com>
2022-07-18 00:49:13 +0800

    test-a8679b54-d96a-4e6c-8916-46302a902a04

--- actions.ps1
+++ actions.ps1
@@ -1,7 +1,7 @@
 Push-Location $PSScriptRoot
 
-# git clone https://github.com/coredns/coredns.git
-# git clone https://github.com/qzlin/forward.git
+git clone https://github.com/coredns/coredns.git
+git clone https://github.com/qzlin/forward.git
 
 # local test repo cache
 if (Test-Path local_cache) {
